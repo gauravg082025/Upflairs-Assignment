322172d70c13103999089000051de31b14166481
gauravg082025 <gauravgarg082025@gmail.com>
2025-07-15 21:21:15 +0530

    (assignment)

---
+++ 12th July (if-else practise).txt/ans.js
@@ -0,0 +1,22 @@
+// // //1. Even or Odd Checker
+// // Write a Node.js script that takes a number as input and determines whether the number is even or odd using if-else.
+
+// let a=6;
+// if(a%2==0){
+//     console.log("The number is even ")
+// }else if (a%2!=0){
+//     console.log("The number is odd")
+// }
+
+
+// //2. Positive, Negative, or Zero
+// Ask the user to input a number. Use if-else conditions to display whether the number is positive, negative, or zero.
+
+let a = -5;
+if(a > 0){
+    console.log("Positive")
+} else if(a==0){
+    console.log("Zero")
+}else if(a<0){
+    console.log("negative")
+}
